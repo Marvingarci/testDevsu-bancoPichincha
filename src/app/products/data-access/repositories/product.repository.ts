725322b69a8241caedfa0328d6f6d5f51e378634
Marvin Alejandro Garcia Gonzalez <marvin.garcia@ad.bantrab.hn>
2023-11-21 16:22:44 -0600

    Service repository implementation

--- src/app/products/data-access/repositories/product.repository.ts
+++ src/app/products/data-access/repositories/product.repository.ts
@@ -1,9 +1,9 @@
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
 
-export abstract class ProductRepository {
-  abstract getProducts(): Observable<Product[]>;
-  abstract createProduct(product: Product): Observable<Product>;
-  abstract updateProduct(product: Product): Observable<Product>;
-  abstract deleteProduct(idProduct: Product): void;
+export interface ProductRepository {
+    getProducts(): Observable<Product[]>;
+    createProduct(product: Product): Observable<Product>;
+    updateProduct(product: Product): Observable<Product>;
+    deleteProduct(idProduct: string): Observable<any>;
 }
--- src/app/products/data-access/services/product.service.ts
+++ src/app/products/data-access/services/product.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { ProductRepository } from '../repositories/product.repository';
+import { Product } from '../models/product';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'env.development';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
+export class ProductService implements ProductRepository {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  createProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(`${environment.URI}/bp/products`, product); 
+  }
+
+  deleteProduct(idProduct: string): Observable<any> {
+    return this.http.delete<any>(`${environment.URI}/bp/products?id=${idProduct}`); 
+  }
+
+  getProducts(): import("rxjs").Observable<Product[]> {
+    return this.http.get<Product[]>(`${environment.URI}/bp/products`); 
+  }
+
+  updateProduct(product: Product): Observable<Product> {
+    return this.http.put<Product>(`${environment.URI}/bp/products`, product); 
+  }
+
+
+
 }
